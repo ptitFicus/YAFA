7cea197d763dbbf5fb3f6c8608fefdec00083ae4
Benjamin Cavy <benjamin.cavy@gmail.com>
2017-01-24 18:53:28 +0100

    Complete example test

--- examples/authErrorWrapper.spec.js
+++ examples/authErrorWrapper.spec.js
@@ -1,8 +1,16 @@
+/* eslint-disable no-unused-expressions */
 import fetch from 'isomorphic-fetch'
 import { startServer, stopServer } from 'http-status-mock'
 
+import sinonChai from 'sinon-chai'
+import sinon from 'sinon'
+import chai from 'chai'
+
 import authErrorWrapper from './authErrorWrapper'
 import wFetch from '../src/'
+
+chai.should()
+chai.use(sinonChai)
 
 
 describe('Auth error wrapper', () => {
@@ -20,6 +28,17 @@ describe('Auth error wrapper', () => {
   it('Should execute callback on 403 status', done => {
     const newFetch = wFetch.wrapResponse(authErrorWrapper(done))
     newFetch('http://localhost:3000/403')
+  })
+
+  it('Should not execute callback on other status', done => {
+    let callback = () => {}
+    callback = sinon.spy(callback)
+    const newFetch = wFetch.wrapResponse(authErrorWrapper(callback))
+    newFetch('http://localhost:3000/404')
+      .then(() => {
+        callback.should.have.not.been.called
+        done()
+      })
   })
 
   after(() => stopServer(server))
